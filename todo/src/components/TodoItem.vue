b960775cbd0c8ecdf03fd544be187500c2fd8a03
Tommy <tommykindle83@gmail.com>
2020-04-11 07:39:23 -0700

    set up inital todoItem with styling

---
+++ todo/src/components/TodoItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <p>Todo Item</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem"
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px #ccc dotted;
+}
+.is-complete {
+  text-decoration: line-through;
+}
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
+</style>

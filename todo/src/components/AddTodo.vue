298c687999164629b0718232a37117de39d7be4b
Tommy <tommykindle83@gmail.com>
2020-04-12 20:14:58 -0700

    cleaned up file structure

--- todo/src/components/AddTodo.vue
+++ todo/src/components/AddTodo.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <form @submit="addTodo">
-      <input
-        type="text"
-        v-model="title"
-        name="title"
-        placeholder="Add Todo..."
-      />
+      <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
       <input type="submit" value="Submit" class="btn" />
     </form>
   </div>
